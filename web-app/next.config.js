a1b66bb1d375c5180019ffd7c4f2a86429cc05f8
Peppy Sisay <peppy@dave.com>
2020-04-19 13:57:12 -0700

    Add deployment

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -2,6 +2,12 @@ const Dotenv = require('dotenv-webpack');
 const withTM = require('next-transpile-modules')(['react-responsive-music-player']);
 
 module.exports = withTM({
+  exportTrailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': {page: '/'}
+    };
+  },
   webpack: (config) => {
     config.plugins.push(new Dotenv({silent: true}));
 
